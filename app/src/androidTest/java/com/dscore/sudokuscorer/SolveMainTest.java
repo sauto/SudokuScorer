49d17df079918e8899faad5069c7e0239f7b6b27
RJ <nositohi@live.jp>
2020-09-05 00:08:14 +0900

    SolveMainの変更後テスト修正漏れ

--- app/src/androidTest/java/com/dscore/sudokuscorer/SolveMainTest.java
+++ app/src/androidTest/java/com/dscore/sudokuscorer/SolveMainTest.java
@@ -5,48 +5,34 @@ import android.util.Log;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.List;
-
 public class SolveMainTest {
 
     @Test
     public void solve() {
-        int[][] mat = Utility.ArrayDimConvertToTwo(UtilityTest.GetSequence(R.string.hard));
-
         SolveMain solveMain = new SolveMain();
         Resources resources = SupportApplication.getContext().getResources();
         String dataList= resources.getString(R.string.hard);
-        List<String> list = solveMain.Solve(dataList,true,true);
-
-        String str ="";
-        for(String s :list )
-            str += s;
+        String str = solveMain.Solve(dataList,true,true);
 
         Log.d("スコア",String.format("%d",solveMain.getDScore()));
         Log.d("解法",solveMain.getLog());
         Assert.assertEquals(str,resources.getString(R.string.hard_answer));
 
         dataList= resources.getString(R.string.veryhard);
-        list = solveMain.Solve(dataList,true,true);
+        str = solveMain.Solve(dataList,true,true);
 
-        str="";
-        for(String s :list )
-            str += s;
 
         Log.d("スコア",String.format("%d",solveMain.getDScore()));
         Log.d("解法",solveMain.getLog());
         Assert.assertEquals(str,resources.getString(R.string.veryhard_answer));
 
         dataList= resources.getString(R.string.veryhard);
-        list = solveMain.Solve(dataList,false,true);
+        str = solveMain.Solve(dataList,false,true);
 
-        str="";
-        for(String s :list ) str += s == "" ? "0" : s;
 
         Log.d("スコア",String.format("%d",solveMain.getDScore()));
         Log.d("解法",solveMain.getLog());
-        Assert.assertEquals(str,resources.getString(R.string.veryhard_no_brute));
+        Assert.assertEquals(str,resources.getString(R.string.veryhard_no_brute_solve_return));
 
     }
 }
